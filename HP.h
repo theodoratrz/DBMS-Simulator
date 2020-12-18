e360a49a530e502106da3631a83e051c5ae89826
pspanoudakis <pavlos.spanoudakis@gmail.com>
2020-12-18 18:13:08 +0200

    Organizing

--- HP.h
+++ HP.h
@@ -16,7 +16,6 @@
 #define HP_INFO_SIZE 12
 #define RECORD_SIZE 94
 #define MAX_RECORDS 5
-#define RECORD_KEY "id"
 
 typedef struct{
     int id;
@@ -29,9 +28,10 @@ typedef struct {
     int fileDesc;       /* File Descriptor */
     char attrType;      /* Key type: 'c', 'i' */
     char *attrName;     /* Key field name */
-    int attrLength;     /* Key length (in bytes?) */
+    int attrLength;     /* Key length (in bytes) */
 } HP_info;
 
+/* General HP functions ----------------------------------------------------------------- */
 
 int HP_CreateFile(  const char *fileName,       /* File name */
                     const char attrType,        /* Key type: 'c', 'i' */
@@ -39,6 +39,8 @@ int HP_CreateFile(  const char *fileName,       /* File name */
                     const int attrLength        /* Key length (in bytes?) */ );
 
 HP_info* HP_OpenFile( const char *fileName );
+
+int HP_InitFile(int fd, char type, const char *name, int length);
 
 int HP_CloseFile( HP_info* header_info );
 
@@ -51,9 +53,15 @@ int HP_DeleteEntry( HP_info header_info,    /* File Header information */
 int HP_GetAllEntries( HP_info header_info,    /* File Header information */
                       void *value             /* Key value of record to be found */ );
 
-void* HP_GetInfoData(HP_info* info);
+/* HP_info functions ----------------------------------------------------------------- */
 
-int HP_InitFile(int fd, char type, const char *name, int length);
+void* HP_GetInfoData(const HP_info* info);
+
+void delete_HP_info(HP_info *info);
+
+HP_info* Get_HP_info(int fd);
+
+/* Block functions ------------------------------------------------------------------- */
 
 void HP_SetNextBlockNumber(void *current, int num);
 
@@ -61,14 +69,6 @@ int HP_GetNextBlockNumber(void *current);
 
 void* HP_GetNextBlock(  int fd, 
                         void *current_block);
-
-void* GetRecordData(const Record *rec);
-
-Record* GetRecord(const void *data);
-
-HP_info* Get_HP_info(int fd);
-
-void* Get_HP_info_Data(const HP_info *info);
 
 void HP_SetNumRecords(void *block, int n);
 
@@ -78,22 +78,26 @@ int HP_InsertRecordtoBlock(int fd, int current_num, Record rec);
 
 int HP_GetNumRecords(void *block);
 
-void PrintRecord(Record rec);
-
-void CopyRecord(void *dest, void *src);
-
-void* NextRecord(void *current);
-
 void* GetLastRecord(void *block);
 
 int HP_DeleteRecordFromBlock(void *block, const char *key_name, void *value);
 
 int BlockHasRecordWithKey(void *block, const char* key_name, Record *rec);
 
-int HP_RecordKeyHasValue(void *record, const char *key_name, void *value);
-
 int PrintBlockRecordsWithKey(void *block, const char *key_name, void *value);
 
-void delete_HP_info(HP_info *info);
+/* Record functions ------------------------------------------------------------------ */
+
+void* GetRecordData(const Record *rec);
+
+Record* GetRecord(const void *data);
+
+void PrintRecord(Record rec);
+
+void CopyRecord(void *dest, void *src);
+
+void* NextRecord(void *current);
+
+int HP_RecordKeyHasValue(void *record, const char *key_name, void *value);
 
 #endif
