c3d6452b257225e5febdd9d27b5357e40e1a35ab
pspanoudakis <pavlos.spanoudakis@gmail.com>
2021-01-12 02:16:53 +0200

    Creating SHT Base & Implementing some functions

--- SHT.c
+++ SHT.c
@@ -33,3 +33,107 @@ int SHT_HashCode(char* data, unsigned long int mod)
     result = hash_num;
     return result;
 }
+
+/* SHT_info functions ------------------------------------------------------------------- */
+
+int SHT_info_size()
+{
+    return sizeof(int) + (strlen("surname") + 1) + sizeof(int) + sizeof(long int) + /*file name size?*/ 15;
+}
+
+void* Get_SHT_info_Data(const SHT_info *info)
+{
+    void *data;
+    void *temp;
+    int name_size = strlen(info->attrName) + 1;
+    int file_name_size = strlen(info->fileName) + 1;
+
+    // Allocating space for the struct
+    data = malloc( SHT_info_size() );
+    temp = data;                    // Used to iterate over the sequence
+
+    // Copying file descriptor
+    memcpy(temp, &(info->fileDesc), sizeof(int));
+    temp = (int*)temp + 1;
+
+    // Copying attribute name
+    memcpy( temp, info->attrName, name_size );
+    temp = (char*)temp + name_size;
+
+    // Copying attribute length
+    memcpy( temp, &(info->attrLength), sizeof(int));
+    temp = (int*)temp + 1;
+
+    // Copying number of buckets
+    memcpy( temp, &(info->numBuckets), sizeof(unsigned long int));
+    temp = (unsigned long int*)temp + 1;
+
+    memcpy( temp, &(info->fileName), file_name_size);
+
+    // Done.
+    return data;
+}
+
+/* Properly deletes the specified SHT_info. */
+void delete_SHT_info(SHT_info *info)
+{
+    free(info->attrName);
+    free(info->fileName);
+    free(info);
+}
+
+/* Returns the header info of the secondary hash file with the specified file descriptor.
+   In case of an error, NULL is returned. */
+SHT_info* Get_SHT_info(int fd)
+{
+    void *block;
+
+    // Return fail if this is not a hash file
+    if ( !IsHashFile(fd) ) { return NULL; }
+
+    // The header is placed in block 0
+    if (BF_ReadBlock(fd, 0, &block) < 0) { return NULL; }
+
+    // The header is a byte sequence, so convert it to a struct HT_info
+    SHT_info *info = malloc( SHT_info_size() );
+    void *temp = (char*)block + strlen("sec_hash") + 1;
+    int len;
+
+    // Copying file descriptor field
+    memcpy(&(info->fileDesc), temp, sizeof(int));
+    temp = (int*)temp + 1;
+
+    // Copying attrName field (size must be determined first)
+    len = strlen((char*)temp) + 1;
+    info->attrName = malloc(len);
+    memcpy(info->attrName, temp, len);
+    temp = (char*)temp + len;
+
+    // Copying attrLength field
+    memcpy(&(info->attrLength), temp, sizeof(int));
+    temp = (int*)temp + 1;
+
+    // Copying numBuckets field
+    memcpy(&(info->numBuckets), temp, sizeof(unsigned long int));
+    temp = (unsigned long int*)temp + 1;
+
+    // Copying fileName field (size must be determined first)
+    len = strlen((char*)temp) + 1;
+    info->fileName = malloc(len);
+    memcpy(info->fileName, temp, len);
+
+    return info;
+}
+
+/* SHT_Record functions -----------------------------------------------------------------*/
+
+int SHT_Record_size()
+{
+    return 25 + sizeof(int);
+}
+
+/* Returns a pointer to the last record of the specified block. */
+void* SHT_GetLastRecord(void *block)
+{
+    return (char*)block + (GetBlockNumRecords(block) - 1)*SHT_Record_size();
+}
--- SHT.h
+++ SHT.h
@@ -7,6 +7,85 @@
 #ifndef SHT_H
 #define SHT_H
 
+#include "HT.h"
+
+typedef struct {
+    Record record;
+    int blockId;
+} SecondaryRecord;
+
+typedef struct {
+    char surname[25];
+    int blockId;
+} SHT_Record;
+
+typedef struct {
+    int fileDesc;
+    char* attrName;
+    int attrLength;
+    long int numBuckets;
+    char* fileName;
+} SHT_info;
+
+int SHT_CreateSecondaryIndex(char *fileName, char attrType, char* attrName, int attrLength, int buckets);
+
+SHT_info* SHT_OpenSecondaryIndex(char *fileName);
+
+int SHT_CloseSecondaryIndex(SHT_info *header_info);
+
+int SHT_SecondaryInsertEntry(SHT_info header_info, Record record);
+
+int SHT_SecondaryGetAllEntries(SHT_info header_info, void *value);
+
+//int HashStatistics(char *filename);
+
+int SHT_InitFile(int fd, char type, const char *name, int length, unsigned long int bucket);
+
+int SHT_CreateBuckets(int fd, int buckets);
+
+int is_SHT_file(int fd);
+
+/* SHT_info functions -------------------------------------------------------------------*/
+
+void delete_SHT_info(SHT_info *info);
+
+void* Get_SHT_info_Data(const SHT_info *info);
+
+SHT_info* Get_SHT_info(int fd);
+
+/* Bucket-Block functions ---------------------------------------------------------------*/
+
+//void SHT_InitBuckets(void *block);
+
+int SHT_InsertEntryToBucket(int fd, int starting_block_num, SHT_Record record, const char *key_name);
+
+int GetAllBucketEntries(int fd, int starting_block_num, void *key_value, const char *key_name);
+
+int GetBucketStats(int fd, int starting_block_num, int *total_blocks, int *total_records);
+
+/* SHT_Record-Block functions -----------------------------------------------------------*/
+
+int SHT_InsertRecordtoBlock(int fd, int block_num, SHT_Record rec);
+
+int SHT_BlockHasRecord(void *block, SHT_Record *rec);
+
+void* SHT_GetLastRecord(void *block);
+
+int SHT_PrintBlockRecordsWithKey(void *block, const char *key_name, void *value);
+
+/* SHT_Record functions -----------------------------------------------------------------*/
+
+int SHT_Record_size();
+
+SHT_Record* Get_SHT_Record(const void *data);
+
+void* get_SHT_Record_data(const Record *rec);
+
+void* Next_SHT_Record(void *current);
+
+int RecordsEqual(void *record, SHT_Record other);
+
+/* The Hash function used by SHT. */
 int SHT_HashCode(char* data, unsigned long int mod);
 
 #endif
